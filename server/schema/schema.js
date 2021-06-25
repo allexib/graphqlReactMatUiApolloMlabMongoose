45483ef90278ff1471862961d2adb6c1aa698392
allexib <ooomegasib@gmail.com>
2021-06-25 13:54:43 +0400

    data migration,cloub mongodb,cluster

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,50 +1,73 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+
+/*
+// All IDs set automatically by mLab
+// Don't forget to update after creation
+const directorsJson = [
+  { "name": "Quentin Tarantino", "age": 55 }, // 60d5a4d4036cfa63e41fce55
+  { "name": "Michael Radford", "age": 72 }, // 60d5a5fd036cfa63e4201d2e
+  { "name": "James McTeigue", "age": 51 }, // 60d5a64e036cfa63e42030e0
+  { "name": "Guy Ritchie", "age": 50 }, // 60d5a662036cfa63e42036c1
+];
+
+// directorId - it is ID from the directors collection
+const moviesJson = [
+  { "name": "Pulp Fiction", "genre": "Crime", "directorId": "60d5a4d4036cfa63e41fce55" },
+  { "name": "1984", "genre": "Sci-Fi", "directorId": "60d5a5fd036cfa63e4201d2e" },
+  { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "60d5a64e036cfa63e42030e0" },
+  { "name": "Snatch", "genre": "Crime-Comedy", "directorId": "60d5a662036cfa63e42036c1" },
+  { "name": "Reservoir Dogs", "genre": "Crime", "directorId": "60d5a4d4036cfa63e41fce55" },
+  { "name": "The Hateful Eight", "genre": "Crime", "directorId": "60d5a4d4036cfa63e41fce55" },
+  { "name": "Inglourious Basterds", "genre": "Crime", "directorId": "60d5a4d4036cfa63e41fce55" },
+  { "name": "Lock, Stock and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": "60d5a662036cfa63e42036c1" },
+];
 
 const movies = [
-    {id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1',},
-    {id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2',},
-    {id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3',},
-    {id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4',},
-    {id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1'},
-    {id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1'},
-    {id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1'},
-    {id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4'},
+  { id: '1', name: "Pulp Fiction", genre: "Crime", directorId: "1" },
+  { id: '2', name: "1984", genre: "Sci-Fi", directorId: "2" },
+  { id: '3', name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: '4', name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: '5', name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
+  { id: '6', name: "The Hateful Eight", genre: "Crime", directorId: "1" },
+  { id: '7', name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  { id: '8', name: "Lock, Stock and Two Smoking Barrels", genre: "Crime-Comedy", directorId: "4" },
 ];
 
 const directors = [
-    {id: '1', name: 'Quentin Tarantino', age: 55},
-    {id: '2', name: 'Michael Radford', age: 72},
-    {id: '3', name: 'James McTeigue', age: 51},
-    {id: '4', name: 'Guy Ritchie', age: 50},
+	{ id: '1', name: "Quentin Tarantino", age: 55 },
+  { id: '2', name: "Michael Radford", age: 72 },
+  { id: '3', name: "James McTeigue", age: 51 },
+  { id: '4', name: "Guy Ritchie", age: 50 },
 ];
+*/
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        genre: {type: GraphQLString},
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
         director: {
             type: DirectorType,
             resolve(parent, args) {
-                return directors.find(director => director.id === parent.id);
-            },
-        },
+                // return directors.find(director => director.id === parent.id);
+            }
+        }
     }),
 });
 
 const DirectorType = new GraphQLObjectType({
     name: 'Director',
     fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        age: {type: GraphQLInt},
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies.filter(movie => movie.directorId === parent.id);
+                // return movies.filter(movie => movie.directorId === parent.id);
             },
         },
     }),
@@ -55,28 +78,28 @@ const Query = new GraphQLObjectType({
     fields: {
         movie: {
             type: MovieType,
-            args: {id: {type: GraphQLID}},
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return movies.find(movie => movie.id === args.id);
+                // return movies.find(movie => movie.id === args.id);
             },
         },
         director: {
             type: DirectorType,
-            args: {id: {type: GraphQLID}},
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return directors.find(director => director.id === args.id);
+                // return directors.find(director => director.id === args.id);
             },
         },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies;
+                // return movies;
             }
         },
         directors: {
             type: new GraphQLList(DirectorType),
             resolve(parent, args) {
-                return directors;
+                // return directors;
             }
         }
     }
